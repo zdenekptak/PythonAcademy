7daea0c2eb7894e97a22da8d99c4fa0a66022008
zdenekptak <zptak@seznam.cz>
2021-01-12 23:38:54 +0100

    #Add guessedNumber() and create main() boot function.

--- BullsAndCows.py
+++ BullsAndCows.py
@@ -1,8 +1,15 @@
 import random
 from datetime import datetime
 
+
+def guessedNumber():
+    digitsList = random.sample(range(10), 4)
+    return digitsList
+
+
 def splitChar(string):
     return [int(char) for char in string]
+
 
 def numberBulls(digitsList, myNumberList):
     bulls = 0
@@ -12,6 +19,7 @@ def numberBulls(digitsList, myNumberList):
                 bulls += 1
     return bulls
 
+
 def numberCows(digitsList, myNumberList):
     cows = 0
     for n in myNumberList:
@@ -19,36 +27,40 @@ def numberCows(digitsList, myNumberList):
             cows += 1
     return cows
 
+
 def myNumber():
     myNumber = input("Enter a number: ")
     myNumberList = splitChar(myNumber)
     return myNumberList
 
 
-print('''
-Hi there!
-I've generated a random 4 digit number for you.
-Let's play a bulls and cows game.
-''')
+def main():
+    print('''
+    Hi there!
+    I've generated a random 4 digit number for you.
+    Let's play a bulls and cows game.
+    ''')
+    digitsList = guessedNumber()
+    bulls = 0
+    count = 0
+    start_time = datetime.now()
+    while bulls < 4:
+        myNumberList = myNumber()
+        if len(myNumberList) == 4:
+            bulls = numberBulls(digitsList, myNumberList)
+            cows = numberCows(digitsList, myNumberList)
+            actualCows = cows - bulls
+            count += 1
+            print(f"Actual bulls is: {bulls} and cows is: {actualCows}")
+        else:
+            print(f"The number is the wrong length, enter four digits.")
+    end_time = datetime.now()
+    duration = end_time - start_time
+    durationModif = str(duration)[:-5]
 
-digitsList = random.sample(range(10), 4)
-print(digitsList)
+    print()
+    print(f"You win, you have {bulls} bulls.")
+    print(f"Yours number of attempts was {count} and time was {durationModif}.")
 
-bulls = 0
-count = 0
-start_time = datetime.now()
 
-while bulls < 4:
-    myNumberList = myNumber()
-    bulls = numberBulls(digitsList, myNumberList)
-    cows = numberCows(digitsList, myNumberList)
-    actualCows = cows - bulls
-    count += 1
-    print(f"Actual bulls is: {bulls} and cows is: {actualCows}")
-
-end_time = datetime.now()
-duration = end_time - start_time
-durationModif = str(duration)[:-5]
-
-print(f"You won, you have {bulls} bulls. You number of attempts is {count}")
-print(f"Yours number of attempts was {count} and time was {durationModif}")
+main()
