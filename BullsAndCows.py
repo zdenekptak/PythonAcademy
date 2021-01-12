d2c9d292ea98427aaa7585b669e5dbce628ef012
zdenekptak <zptak@seznam.cz>
2021-01-12 23:00:43 +0100

    #Adding a time meter

--- BullsAndCows.py
+++ BullsAndCows.py
@@ -1,4 +1,5 @@
 import random
+from datetime import datetime
 
 def splitChar(string):
     return [int(char) for char in string]
@@ -29,13 +30,13 @@ Hi there!
 I've generated a random 4 digit number for you.
 Let's play a bulls and cows game.
 ''')
-import random
 
 digitsList = random.sample(range(10), 4)
 print(digitsList)
 
 bulls = 0
 count = 0
+start_time = datetime.now()
 
 while bulls < 4:
     myNumberList = myNumber()
@@ -45,4 +46,9 @@ while bulls < 4:
     count += 1
     print(f"Actual bulls is: {bulls} and cows is: {actualCows}")
 
+end_time = datetime.now()
+duration = end_time - start_time
+durationModif = str(duration)[:-5]
+
 print(f"You won, you have {bulls} bulls. You number of attempts is {count}")
+print(f"Yours number of attempts was {count} and time was {durationModif}")
