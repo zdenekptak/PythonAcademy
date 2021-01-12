785a3fc94c53382ae69cdbf67bb893f57310f881
zdenekptak <zptak@seznam.cz>
2021-01-12 15:28:48 +0100

    #První funkční verze

--- BullsAndCows.py
+++ BullsAndCows.py
@@ -1,27 +1,48 @@
 import random
 
-
-def splitchar(string):
+def splitChar(string):
     return [int(char) for char in string]
 
+def numberBulls(digitsList, myNumberList):
+    bulls = 0
+    for indexDl, dl in enumerate(digitsList):
+        for indexNl, nl in enumerate(myNumberList):
+            if indexDl == indexNl and dl == nl:
+                bulls += 1
+    return bulls
+
+def numberCows(digitsList, myNumberList):
+    cows = 0
+    for n in myNumberList:
+        if n in digitsList:
+            cows += 1
+    return cows
+
+def myNumber():
+    myNumber = input("Enter a number: ")
+    myNumberList = splitChar(myNumber)
+    return myNumberList
+
+
+print('''
+Hi there!
+I've generated a random 4 digit number for you.
+Let's play a bulls and cows game.
+''')
+import random
 
 digitsList = random.sample(range(10), 4)
 print(digitsList)
 
-myNumber = input("Uhodni čtyrmístné číslo: ")
-myNumberList = splitChar(myNumber)
-print(myNumberList)
-
 bulls = 0
-cows = 0
+count = 0
 
-for indexDl, dl in enumerate(digitsList):
+while bulls < 4:
+    myNumberList = myNumber()
+    bulls = numberBulls(digitsList, myNumberList)
+    cows = numberCows(digitsList, myNumberList)
+    actualCows = cows - bulls
+    count += 1
+    print(f"Actual bulls is: {bulls} and cows is: {actualCows}")
 
-    for indexNl, nl in enumerate(myNumberList):
-        if indexDl == indexNl and dl == nl:
-            bulls += 1
-        elif nl != dl and nl in digitsList:
-            cows += 1
-            print(cows)
-print(bulls)
-print(cows)
+print(f"You won, you have {bulls} bulls. You number of attempts is {count}")
