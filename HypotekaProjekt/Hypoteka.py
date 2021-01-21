5c028ddcd2d4fee6f9e79fe47eb8dd3a142216ad
zdenekptak <zptak@seznam.cz>
2021-01-21 13:56:43 +0100

    #Pridani funkce main pro ovladani programu a funkce pro vypocet mesicni splatky

--- HypotekaProjekt/Hypoteka.py
+++ HypotekaProjekt/Hypoteka.py
@@ -1,3 +1,11 @@
+def main():
+    vstUdajeKHypo = vstupniInformace()
+    pujcka = vstUdajeKHypo[0]
+    urok = vstUdajeKHypo[1]
+    pocetMesicu = vstUdajeKHypo[2] * 12
+
+    splatka = mesicniSplatka(pujcka, urok, pocetMesicu)
+
 def vstupniInformace():
     # Vyse pujcky
     castka = float(input("Kolik si chcete pujcit: "))
@@ -9,3 +17,8 @@ def vstupniInformace():
     print()
     # Vraceni vstupů
     return [castka, urok, roky]
+
+def mesicniSplatka(pujcka, urok, pocetMesicu):
+    r = urok / 100 / 12
+    mesicniSplatka = pujcka*(r * (1 + r)**pocetMesicu) / ((1 + r)**pocetMesicu-1)
+    return mesicniSplatka
