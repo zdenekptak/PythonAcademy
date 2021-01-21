98c96abd7c3c6c69b80efbc421f763ccffe594f7
zdenekptak <zptak@seznam.cz>
2021-01-21 13:58:17 +0100

    #Pridani funkce celkovaSuma a celkovyUrok plus uprava fce main

--- HypotekaProjekt/Hypoteka.py
+++ HypotekaProjekt/Hypoteka.py
@@ -5,6 +5,11 @@ def main():
     pocetMesicu = vstUdajeKHypo[2] * 12
 
     splatka = mesicniSplatka(pujcka, urok, pocetMesicu)
+    print(f"Mesicni splatka je: {splatka}")
+    suma = celkovaSuma(splatka, pocetMesicu)
+    print(f"Celkova suma kterou musime zaplatit: {suma}")
+    urokZaplaceny = celkovyUrok(pujcka, suma)
+    print(f"Celkový úrok který musíme zaplatit: {urokZaplaceny}")
 
 def vstupniInformace():
     # Vyse pujcky
@@ -22,3 +27,9 @@ def mesicniSplatka(pujcka, urok, pocetMesicu):
     r = urok / 100 / 12
     mesicniSplatka = pujcka*(r * (1 + r)**pocetMesicu) / ((1 + r)**pocetMesicu-1)
     return mesicniSplatka
+
+def celkovaSuma(mesicniSplatka, pocetMesicu):
+    return mesicniSplatka * pocetMesicu
+
+def celkovyUrok(pujcka, celkovaSuma):
+    return celkovaSuma - pujcka
